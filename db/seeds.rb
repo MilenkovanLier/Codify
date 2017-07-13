7e842882f393e5ff527603fab1df311c08490cb0
YOUR NAME <muvanlier@gmail.com>
2017-07-13 22:04:25 +0200

    added artist page +associations

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-songs2 = Song.create!( {name: "7 Days"} )
-songs3 = Song.create!( {name: "Oldskool"} )
-songs4 = Song.create!( {name: "Three little birds"} )
-songs5 = Song.create!( {name: "Tennesse"} )
-songs6 = Song.create!( {name: "Changes"} )
-songs7 = Song.create!( {name: "Heroes"} )
-songs8 = Song.create!( {name: "There's a Light That Never Goes Out"} )
+Song.delete_all
+Artist.delete_all
+
+
+artist1 = Artist.create!( {name: "Bob Dylan"} )
+artist2 = Artist.create!( {name: "Craig David"} )
+artist3 = Artist.create!( {name: "Tupac"} )
+artist4 = Artist.create!( {name: "Bob Marley"} )
+artist5 = Artist.create!( {name: "Allen Rayman"} )
+artist6 = Artist.create!( {name: "Charles Bradley"} )
+artist7 = Artist.create!( {name: "David Bowie"} )
+artist8 = Artist.create!( {name: "Jim Morrissey"} )
+
+
+songs1 = Song.create!( {name: "Like a Rolling Stone", artist: artist1})
+songs2 = Song.create!( {name: "7 Days", artist: artist2} )
+songs3 = Song.create!( {name: "Oldskool", artist: artist3} )
+songs4 = Song.create!( {name: "Three little birds", artist: artist4} )
+songs5 = Song.create!( {name: "Tennesse", artist: artist5} )
+songs6 = Song.create!( {name: "Changes",  artist: artist6} )
+songs7 = Song.create!( {name: "Heroes", artist: artist7} )
+songs8 = Song.create!( {name: "There's a Light That Never Goes Out", artist: artist8} )
