099cb931946a3da532aafdbb98ea0d1c543078c4
YOUR NAME <muvanlier@gmail.com>
2017-07-15 12:34:35 +0200

    added show and create method for api

--- app/controllers/api/songs_controller.rb
+++ app/controllers/api/songs_controller.rb
@@ -1,7 +1,38 @@
 class Api::SongsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
     render status: 200, json: {
       songs: Song.all
     }.to_json
+  end
+
+  def show
+    song = Song.find(params[:id])
+
+    render status: 200, json: {
+      song: song
+    }.to_json
+  end
+
+  def create
+    song = Song.new(song_params)
+
+    if song.save
+      render status: 201, json: {
+        message: "Song has been saved", song: song
+      } .to_json
+    else
+      render status: 422, json: {
+        errors: song.errors
+      }.to_json
+    end
+  end
+
+
+private
+
+  def song_params
+    params.require(:song).permit(:name)
   end
 end
