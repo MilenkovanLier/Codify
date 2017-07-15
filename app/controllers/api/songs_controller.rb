6f54c539f0b1dba9a7c266cb307f4aae52f16898
YOUR NAME <muvanlier@gmail.com>
2017-07-15 12:57:50 +0200

    pdated API with serialize

--- app/controllers/api/songs_controller.rb
+++ app/controllers/api/songs_controller.rb
@@ -2,32 +2,26 @@ class Api::SongsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
-    render status: 200, json: {
-      songs: Song.all
-    }.to_json
+    songs = Song.all
+    render status: 200, json: songs
   end
 
   def show
     song = Song.find(params[:id])
-
-    render status: 200, json: {
-      song: song
-    }.to_json
+    render status: 200, json: song
   end
 
   def create
     song = Song.new(song_params)
 
     if song.save
-      render status: 201, json: {
-        message: "Song has been saved", song: song
-      } .to_json
+      render status: 200, json: song
     else
       render status: 422, json: {
         errors: song.errors
-      }.to_json
+      }
     end
-  end 
+  end
 
 
 private
