bb4171db78bd9b1a4bc67f5cb4f763c6007c654f
YOUR NAME <muvanlier@gmail.com>
2017-07-16 21:23:33 +0200

    saved the last changes. Stuck as a bird in cage

--- app/assets/javascripts/songs.js
+++ app/assets/javascripts/songs.js
@@ -1,13 +1,41 @@
 
   $(document).ready(function() {
-
-    $('input[type=radio][name=new-song]').change(function(e)) {
-      console.log(e.target.value);
-    })
+    console.log();
 
 
+      function submitTodo(event) {
+        // stop the form from doing the default action, submitting...
+        event.preventDefault();
+        var title = $("#new-todo").val();
+        createTodo(title);
+        $("#newSongTitle").val(null);
+        }
+
+      function createSong(title) {
 
 
+        var listItem = $("<li></li>");
+        listItem.addClass("songs");
+
+
+        var label = $('<label></label>');
+        label.html(title);
+        listItem.append(label);
+
+        $("#todolist").append( listItem );
+
+        updateCounters();
+      }
+
+      function nextSongId() {
+        return $(".todo").length + 1;
+      }
+      // Every todo has it's own id so we can add that to the corresponding label's
+      // "for" attribute to make sure that when we click the label, the checkbox
+      // toggles
+      function nextTodoId() {
+        return document.getElementsByClassName("songs").length + 1;
+}
 
 
   });
